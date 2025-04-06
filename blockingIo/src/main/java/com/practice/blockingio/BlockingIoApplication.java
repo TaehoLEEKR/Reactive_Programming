b9af41548403687788f8971a976bd1f1aaeea40e
TaehoLEEKR <679748@naver.com>
2025-04-06 16:32:36 +0900

    first project

---
+++ blockingIo/src/main/java/com/practice/blockingio/BlockingIoApplication.java
@@ -0,0 +1,14 @@
+package com.practice.blockingio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingIoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BlockingIoApplication.class, args);
+    }
+
+}
---
+++ blockingIo/src/test/java/com/practice/blockingio/BlockingIoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.practice.blockingio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockingIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
